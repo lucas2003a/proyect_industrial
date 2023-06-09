25d506ed944d3f6544948532d00fb98cb9f2ae27
lucas <lucasatuncar1@gmail,com>
2023-06-09 12:41:47 -0500

    diseño navbar

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -18,44 +19,42 @@
     <!-- place navbar here -->
   </header>
   <main>
+
   </main>
-    <section class="vh-100 gradient custom">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-6 col-xl5">
-            <div class="card bg-primary text-white">
-              <div class="card-body p-5 text-center" style="border-radius: 1rem;">
-                <div class="mb-md-5 mt-md-4 pb-5">
-                  <h1 class="fw-bold mb-2 text-uppercase">login</h1>
-                  <p class="text-white-50 mb-5">Ingrese sus credenciales por favor</p>
-                  <div class="form-outline form-white mb-4">
-                    <input type="email" id="emailX" class="form-control form-control-lg">
-                    <label for="emailX" class="form-label">Email</label>
-                  </div>
-                  <div class="form-otuline form-white mb-4">
-                    <input type="password" id="passwordX" class="form-control form-control-lg">
-                    <label for="passwordX" class="form-label">Contraseña</label>
-                  </div>
-                  <p class="small mb-5 pb-lg-2"><a href="#" class="text-white-50">Olvidaste la contraseña?</a></p>
-                  <button class="btn btn-outline-light btn-lg px-5">Login</button>
-                  <div class="d-flex justify-content-center text-center mt4 pt-1">
-                    <a href="#" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                    <a href="#" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                    <a href="#" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                  </div>
-                  <div>
-                    <p class="mb=0">No tienes una cuenta? <a href="#" class="text-white-50 fw-bold">Registrate</a></p>
-                  </div>
-                </div>
-              </div>
-              </div>
+    <!--<div class="bg-image"></div>-->
+    <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block" style="z-index: 2000;">
+          <div class="container-fluid">
+            <a class="navbar-brand nav-link" target ="_blank" href="#"><strong>Industrial</strong></a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-flex flex-row">    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Action 1</a>
+                            <a class="dropdown-item" href="#">Action 2</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="d-flex my-2 my-lg-0">
+                    <input class="form-control me-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
-    </section>
+    </nav>
+    
   <footer>
     <!-- place footer here -->
   </footer>
@@ -66,7 +65,6 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-
   </script>
 </body>
 
---
+++ views/index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>
+  </main>
+    <section class="vh-100 gradient custom">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl5">
+            <div class="card bg-primary text-white">
+              <div class="card-body p-5 text-center" style="border-radius: 1rem;">
+                <div class="mb-md-5 mt-md-4 pb-5">
+                  <h1 class="fw-bold mb-2 text-uppercase">login</h1>
+                  <p class="text-white-50 mb-5">Ingrese sus credenciales por favor</p>
+                  <div class="form-outline form-white mb-4">
+                    <input type="email" id="emailX" class="form-control form-control-lg">
+                    <label for="emailX" class="form-label">Email</label>
+                  </div>
+                  <div class="form-otuline form-white mb-4">
+                    <input type="password" id="passwordX" class="form-control form-control-lg">
+                    <label for="passwordX" class="form-label">Contraseña</label>
+                  </div>
+                  <p class="small mb-5 pb-lg-2"><a href="#" class="text-white-50">Olvidaste la contraseña?</a></p>
+                  <button class="btn btn-outline-light btn-lg px-5">Login</button>
+                  <div class="d-flex justify-content-center text-center mt4 pt-1">
+                    <a href="#" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                  </div>
+                  <div>
+                    <p class="mb=0">No tienes una cuenta? <a href="#" class="text-white-50 fw-bold">Registrate</a></p>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+
+  </script>
+</body>
+
+</html>
