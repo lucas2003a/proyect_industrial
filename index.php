91b350fd3685efda9ca53478d344ae0d7a6c5fb1
lucas <lucasatuncar1@gmail,com>
2023-06-15 12:53:43 -0500

    concept_body

--- index.php
+++ index.php
@@ -21,9 +21,41 @@
 
 </head>
 
-<body>
-  <header>
-    <!-- place navbar here -->
+<body id="page-top">
+  <!--Navitagion-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="#page-top">Inicio</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
+          aria-expanded="false" aria-label="Toggle navigation">
+          Menú
+          <i class="fas fa-bars"></i>
+          <!--puedes usar  este otro icono<span class="navbar-toggler-icon"></span>-->
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#conocenos">Conocenos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#trabajos">Trabajos</a>
+            </li>
+              <a class="nav-link" href="#contacto">Contacto</a>
+            </li>
+          </ul>
+      </div>
+    </nav>
+    <!--Masthead-->
+  <header class="masthead">
+    <div class="container px-4 px-lg-5 h-100 align-items-center justify-content-center">
+      <div class="d-flex justify-content-center">
+        <div class="text-center">
+          <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+          <a href="" class="btn btn-primary" href="#conocenos">¡Comienza ya!</a>
+        </div>
+      </div>
+    </div>
   </header>
   <main>
 
