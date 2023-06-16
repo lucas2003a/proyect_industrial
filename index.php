c0af1b9957cc5bb79496a8f60cabed5dfaea3ee9
lucas <lucasatuncar1@gmail,com>
2023-06-16 10:28:52 -0500

    2 concept_body

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
   <meta name="author" content="" />
   <title>Gray scale - Start bootstrap Theme</title>
   <link rel="icon" type="image/x-icon" href="views/img/descarga-removebg-preview.png">
+  <!-- Font Awesome icons (free version)-->
   <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
   <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+  <!-- Core theme CSS (includes Bootstrap)-->
   <link href="css/style.css" rel="stylesheet"/>
 
   <!-- Bootstrap CSS v5.2.1 -->
@@ -24,7 +27,7 @@
 <body id="page-top">
   <!--Navitagion-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container px-4 px-lg-5">
+      <div class="container px-4 px-lg-5">
         <a class="navbar-brand" href="#page-top">Inicio</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
           aria-expanded="false" aria-label="Toggle navigation">
@@ -40,23 +43,25 @@
             <li class="nav-item">
               <a class="nav-link" href="#trabajos">Trabajos</a>
             </li>
+            <li class ="nav-item">
               <a class="nav-link" href="#contacto">Contacto</a>
             </li>
           </ul>
+        </div>
       </div>
     </nav>
     <!--Masthead-->
-  <header class="masthead">
-    <div class="container px-4 px-lg-5 h-100 align-items-center justify-content-center">
-      <div class="d-flex justify-content-center">
-        <div class="text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
-          <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-          <a href="" class="btn btn-primary" href="#conocenos">¡Comienza ya!</a>
+    <header class="masthead">
+      <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+        <div class="d-flex justify-content-center">
+          <div class="text-center">
+            <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
+            <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
+            <a class="btn btn-primary" href="#conocenos">¡Comienza ya!</a>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
   <main>
 
   </main>
