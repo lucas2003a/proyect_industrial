055e42298dc9dc74aad282155c1829a867f2de46
lucas <lucasatuncar1@gmail,com>
2023-06-19 12:47:38 -0500

    error solucionado

--- index.php
+++ index.php
@@ -82,47 +82,48 @@
     <!--trabajos-->
     <section class="trabajos-section bg-ligth" id="trabajos">
       <div class="container px-4 px-lg-5">
-      <!--proyecto presentado-->
+        <!--proyecto presentado-->
         <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
           <div class="col-xl-8 col-lg-7">
             <img class="img-fluid mb-3 mb-lg-0" src="views/img/bg-masthead.jpg" alt="...">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="presentado-text text-center">
-            <h4>Shoreline</h4>
-            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="presentado-text text-center">
+              <h4>Shoreline</h4>
+              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+            </div>
           </div>
         </div>
-      </div>
-      <!---project one row-->
-      <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-        <div class="col-lg-6"><img class="img-fluid" src="views/img/demo-image-01.jpg" alt="..."></div>
-        <div class="col-lg-6">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Misty</div>
-                <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description</p>
+        <!---project one row-->
+        <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+          <div class="col-lg-6"><img class="img-fluid" src="views/img/demo-image-01.jpg" alt="..."></div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Misty</h4>
+                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <!--project two row-->
-      <div class="row gx-0 justify-content-center">
-        <div class="col-lg-6">
-          <img class="img-fluid" src="views/img/demo-image-02.jpg" alt="...">
-        </div>
-        <div class="col-lg-6 order-lg-first">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-rigth">
-                <h4 class="text-white">Mountains</h4>
-                <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well!</p>
+        <!--project two row-->
+        <div class="row gx-0 justify-content-center">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="views/img/demo-image-02.jpg" alt="...">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-rigth">
+                  <h4 class="text-white">Mountains</h4>
+                  <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well!</p>
+                </div>
               </div>
             </div>
-          </div>
-        </div>        
+          </div>        
+        </div>
       </div>
     </section>
     <!--singup-->
