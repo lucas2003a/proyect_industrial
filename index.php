102941a39bf2f68bf64fd86d87784d25bd039645
lucas <lucasatuncar1@gmail,com>
2023-06-19 12:17:57 -0500

    html acabado con bootstrap

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
               <a class="nav-link" href="#trabajos">Trabajos</a>
             </li>
             <li class ="nav-item">
-              <a class="nav-link" href="#contacto">Contacto</a>
+              <a class="nav-link" href="#singup">Contacto</a>
             </li>
           </ul>
         </div>
@@ -76,7 +76,7 @@
               </p>
             </div>
           </div>
-          <img class="img-fluid" src="views/img/industrial.jpg" alt="...">
+          <img class="img-fluid" src="views/img/ipad.png" alt="...">
       </div>
     </section>
     <!--trabajos-->
@@ -85,7 +85,7 @@
       <!--proyecto presentado-->
         <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
           <div class="col-xl-8 col-lg-7">
-            <img class="img-fluid mb-3 mb-lg-0" src="views/img/industrial.jpg" alt="...">
+            <img class="img-fluid mb-3 mb-lg-0" src="views/img/bg-masthead.jpg" alt="...">
         </div>
         <div class="col-xl-4 col-lg-5">
           <div class="presentado-text text-center">
@@ -96,12 +96,12 @@
       </div>
       <!---project one row-->
       <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-        <div class="col-lg-6"><img class="img-fluid" src="views/img/industrial2.jpg" alt="..."></div>
+        <div class="col-lg-6"><img class="img-fluid" src="views/img/demo-image-01.jpg" alt="..."></div>
         <div class="col-lg-6">
           <div class="bg-black text-center h-100 project">
             <div class="d-flex h-100">
               <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Mishty</div>
+                <h4 class="text-white">Misty</div>
                 <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description</p>
               </div>
             </div>
@@ -111,7 +111,7 @@
       <!--project two row-->
       <div class="row gx-0 justify-content-center">
         <div class="col-lg-6">
-          <img class="img-fluid" src="views/img/industrial3.jpg" alt="...">
+          <img class="img-fluid" src="views/img/demo-image-02.jpg" alt="...">
         </div>
         <div class="col-lg-6 order-lg-first">
           <div class="bg-black text-center h-100 project">
@@ -125,11 +125,118 @@
         </div>        
       </div>
     </section>
-  <main>
-
-  </main>
-  <footer>
-    <!-- place footer here -->
+    <!--singup-->
+    <section class="singup-section" id="singup">
+      <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+            <i class="far fa-paper-plane fa-2x mb-2 text-black"></i>
+            <h2 class="text-black mb-5">Subscribete para recibir actualizaciones</h2>
+            <!-- * * * * * * * * * * * * * * *-->
+            <!-- * * SB Forms Contact Form * *-->
+            <!-- * * * * * * * * * * * * * * *-->
+            <!-- This form is pre-integrated with SB Forms.-->
+            <!-- To make this form functional, sign up at-->
+            <!-- https://startbootstrap.com/solution/contact-forms-->
+            <!-- to get an API token!-->
+            <form class="form-singup" id="contacForm" data-sb-form-api-token="API_TOKEN">
+              <!--Email address input-->
+              <div class="row input-group-newsletter">
+                <div class="col">
+                  <input class="form-control" id="emailAddress" type="email" placeholder="Ingresa tu dirección email ..." arial-label="Ingresa tu dirección email ..." data-sb-validations="required,email">
+                </div>
+                <div class="col-auto">
+                  <button class="btn btn-disabled" id="submitButton" type="submit">Notify me</button>
+                </div>  
+              </div>
+              <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:required">An email is required</div>
+              <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:email">Email is not valid.</div>
+              <!-- Submit success message-->
+              <!---->
+              <!-- This is what your users will see when the form-->
+              <!-- has successfully submitted-->
+              <div class="d-none" id="submitSuccessMessage">
+                <div class="text-center mb-3 mt-2 text-black">
+                  <div class="fw-bolder">Subscripción a formulario exitosa!</div>
+                  Para activar ete formulario, registrate en
+                  <br>
+                  <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                </div>
+              </div>
+              <!-- Submit error message-->
+              <!---->
+              <!-- This is what your users will see when there is-->
+              <!-- an error submitting the form-->
+              <div class="d-none" id="submitErrorMessage">
+                <div class="text-center text-danger mb-3 mt-2">Error sending message!</div>
+              </div>   
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--Contacto-->
+    <section class="contacto-section bg-black">
+      <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5">
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-header">
+                Header
+              </div>
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Dirección</h4>
+                <hr class="my-4 mx-auto">
+                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+              </div>
+              <div class="card-footer text-muted">
+                Footer
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-header">
+                Header
+              </div>
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4 mx-auto">
+                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+              </div>
+              <div class="card-footer text-muted">
+                Footer
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-header">
+                Header
+              </div>
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">teléfono</h4>
+                <hr class="my-4 mx-auto">
+                <div class="small text-black-50">+1 (555) 902-8832</div>
+              </div>
+              <div class="card-footer text-muted">
+                Footer
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="social d-flex justify-content-center">
+          <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+          <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+          <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+        </div>
+      </div>
+    </section>
+  <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2023</div>
+  <!-- place footer here -->
   </footer>
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
@@ -139,6 +246,15 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
   </script>
+  <!-- Bootstrap core JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Core theme JS-->
+  <script src="js/scripts.js"></script>
+  <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+  <!-- * *                               SB Forms JS                               * *-->
+  <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+  <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+  <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 
 </html>
