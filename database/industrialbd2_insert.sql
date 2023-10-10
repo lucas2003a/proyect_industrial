254ab57cc8ed7e93f84ac44574ef6c372cd7c8ae
lucas2003 <lucasatuncar1@gmail.com>
2023-10-10 12:48:48 -0500

    spu_accesos acabados

--- database/industrialbd2_insert.sql
+++ database/industrialbd2_insert.sql
@@ -2,5 +2,9 @@ use industrialbd2;
 
 -- ACCESOS
 
-call accesos_listar();
-call accesos_registrar('AC001','REGISTRAR ALUMNO','ADMIN');
+select * from accesos;
+call spu_acceso_listar();
+call spu_acceso_registrar('AC001','LISTAR ALUMNO','ADMIN');
+call spu_acceso_modificar(2,'ACC03','LISTAR ALUMNO','','','USERMASTER');
+call spu_acceso_eliminar(2);
+call spu_acceso_reactivar(2);
--- database/industrialbd2_spu.sql
+++ database/industrialbd2_spu.sql
@@ -2,18 +2,20 @@ use industrialbd2;
 
 -- ACCESOS //////////
 
-drop procedure if exists accesos_listar;
+drop procedure if exists spu_acceso_listar;
 delimiter $$
-create procedure accesos_listar()
+create procedure spu_acceso_listar()
 begin
 	select * from accesos
     where estado = '1';
 end $$
 delimiter ;
 
-drop procedure if exists accesos_registrar;
+-- ///////
+
+drop procedure if exists spu_acceso_registrar;
 delimiter $$
-create procedure accesos_registrar
+create procedure spu_acceso_registrar
 (
 	in _codigoacceso	char(5),
     in _nombreacceso	varchar(20),
@@ -27,9 +29,54 @@ begin
 end $$
 delimiter ;
 
-drop procedure if exists accesos_modificar;
+-- ///////
+
+drop procedure if exists spu_acceso_modificar;
 delimiter $$
-create procedure accesos_modificar()
+create procedure spu_acceso_modificar
+(
+	in _idacceso		int,
+	in _codigoacceso	char(5),
+    in _nombreacceso	varchar(20),
+    in _date_update		date,
+    in _date_delete		date,
+    in _usuario			varchar(20)
+)
 begin
+ update accesos set
+	codigoacceso 	= _codigoacceso,
+    nombreacceso 	= _nombreacceso,
+    date_update 	= nullif(_date_update,''),
+    date_delete 	= nullif(_date_delete,''),
+    usuario 		= _usuario,
+    date_set = now()
+    where
+		idacceso = _idacceso;
 end $$
-delimiter ;()
+delimiter ;
+
+-- ////////
+drop procedure if exists spu_acceso_eliminar;
+delimiter $$
+create procedure spu_acceso_eliminar(in _idacceso int)
+begin
+ update accesos set
+	date_delete = now(),
+	estado = '0'
+where 
+	idacceso = _idacceso;
+end $$
+delimiter ;
+
+-- ////
+drop procedure if exists spu_acceso_reactivar;
+delimiter $$
+create procedure spu_acceso_reactivar(in _idacceso int)
+begin 
+	update accesos set
+		date_delete = null,
+        estado = '1'
+	where
+		idacceso = _idacceso;
+end $$
+delimiter ;
